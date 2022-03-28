9b1f5e4b8519d741ddebc9ced0a647633c311080
Amen Park <86364207+AmenPark@users.noreply.github.com>
2022-03-28 23:53:10 +0900

    Update solution2.py

--- 문자열의 아름다움/solution2.py
+++ 문자열의 아름다움/solution2.py
@@ -1,12 +1,12 @@
-def get_num(k, i, iternum):
-    # SUM<(k-i)**2 - (k-i)> = k**2 - 2ki + i**2 - k + i for i = 0,...,n-1. iternum = n
-    #                       = k**2 - k + i**2 + i(1 - 2k)
+def get_num(k, i,  iternum):
+    # SUM<(k-ai)**2 - (k-ai)> = k**2 - 2kai + a**2 * i**2 - k + ai for i = 0,...,n-1. iternum = n
+    #                       = k**2 - k + a^2 i**2 + a * i(1 - 2k)
     var = (k**2 - k) * iternum
-    var += (1 - 2 * k) * (iternum * (iternum-1) // 2)
-    var += iternum * (iternum - 1) * (2* iternum - 1) // 6
+    var += (1 - 2 * k) * (iternum * (iternum-1) // 2) * i
+    var += iternum * (iternum - 1) * (2* iternum - 1) // 6 * i * i
     return var // 2       # 나누기 2는 기본으로 했어야 했던 것.
 
-def solution(s):
+def solutionb(s):
     N = len(s)
     # answer = SUM<N * (N+1) // 2> for N = 1~N
     answer = ((N * (N-1) * (2*N - 1))//6 + N * (N-1) // 2)//2
@@ -46,9 +46,8 @@ def solution(s):
             deltakey = chr_conti - before_key
             before_key = chr_conti
             # SUM<(k-i)**2 - (k-i) = k**2 - 2ki + i**2 - k + i> for i = 0,...,?
-            var += get_num(chr_num,conti_ctnum,deltakey)            
+            var += get_num(chr_num,conti_ctnum ,deltakey)            
             chr_num -= conti_ctnum * deltakey
             conti_ctnum -=  conti_num
         answer -= var
-            
     return answer
